3a70dbecaf4fd4e90b573d0a2812d6d350ab1ebd
Chamin Im <ckals413@naver.com>
2025-10-16 21:46:21 +0900

    :sparkles:[feat] : 로그인 시 비밀번호 가림

--- app/src/main/java/com/sopt/dive/login/LoginActivity.kt
+++ app/src/main/java/com/sopt/dive/login/LoginActivity.kt
@@ -32,6 +32,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -153,7 +155,8 @@ fun Greeting(modifier: Modifier = Modifier,
                 Modifier.fillMaxWidth(),
                 placeholder = { Text(stringResource(R.string.pw_placeholder)) },
                 singleLine = true,
-                colors = TextFieldDefaults.colors(
+                visualTransformation = PasswordVisualTransformation(),
+                        colors = TextFieldDefaults.colors(
                     focusedContainerColor = Color.Transparent,      // 포커스 시
                     unfocusedContainerColor = Color.Transparent,    // 포커스 해제 시
                     disabledContainerColor = Color.Transparent,     // 비활성 시
